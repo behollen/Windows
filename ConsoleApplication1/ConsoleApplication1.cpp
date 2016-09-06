f0f7be0426d01fcaf58957ce54774ee43060fe30
behollen <jm82.seo@samsung.com>
2016-09-06 20:46:55 +0900

    First submit my test codes

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+// ConsoleApplication1.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	cout << "Please input password : ";
+
+	cin >> a;
+	cout << "Number : " << a << endl;
+    return 0;
+}
+
